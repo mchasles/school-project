083f66d2c8994e07208ef4fdb1aef3cc714fb0ae
Maxime Chasles <maxime.chasles@gmail.com>
2019-09-20 23:42:29 +0200

    students list with hardcoded fixture

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import Avatar from "@material-ui/core/Avatar";
+import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import students from "./temp_fixture";
 
 function App() {
   return (
-    <div></div>
+    <>
+      <CssBaseline />
+      <Container maxWidth="false">
+        <List>
+          {students.students.map(student => (
+            <>
+              <ListItem alignItems="center">
+                <ListItemAvatar>
+                  <Avatar alt={student.name} src={student.avatar} />
+                </ListItemAvatar>
+                <ListItemText primary={student.name} />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+            </>
+          ))}
+        </List>
+      </Container>
+    </>
   );
 }
 
