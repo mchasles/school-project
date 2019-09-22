b9a710a6e15d62978b7913cdf5bb68c9a43dd849
Maxime Chasles <maxime.chasles@gmail.com>
2019-09-22 13:51:29 +0200

    add app loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { useSelector, useDispatch } from 'react-redux';
@@ -24,6 +24,7 @@ import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Fab from '@material-ui/core/Fab';
 import IconButton from '@material-ui/core/IconButton';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
@@ -33,41 +34,65 @@ import Tooltip from '@material-ui/core/Tooltip';
 
 import StudentForm from './StudentForm.component';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  loader: {
+    position: 'absolute',
+    top: 0,
+    width: '100vw'
+  },
+  container: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
   const students = useSelector(state => state.students);
   const currentEditId = useSelector(state => state.currentEditId);
   const addingStudent = useSelector(state => state.addingStudent);
 
   const dispatch = useDispatch();
-  const fetchAllStudents = useCallback(() => dispatch(fetchStudents()), [
-    dispatch
-  ]);
+
+  const wrapLoading = asyncFn => (...args) => {
+    setLoading(true);
+    async function load() {
+      await asyncFn(...args);
+      setLoading(false);
+    }
+    load();
+  };
 
   useEffect(() => {
-    fetchAllStudents();
-  }, [fetchAllStudents]);
+    wrapLoading(() => dispatch(fetchStudents()))();
+  }, [dispatch]);
 
   const openAdd = () => dispatch(addStudentOpen());
   const cancelAdd = () => dispatch(addStudentClose());
-  const submitAddStudent = student => dispatch(addStudent(student));
+  const submitAddStudent = wrapLoading(student =>
+    dispatch(addStudent(student))
+  );
 
   const openEdit = id => dispatch(editStudentOpen(id));
   const cancelEdit = () => dispatch(editStudentClose());
-  const submitEditStudent = (student, name) => {
+  const submitEditStudent = wrapLoading(student =>
     dispatch(
       editStudent(student.id, {
-        ...student,
-        name
+        ...student
       })
-    );
-  };
+    )
+  );
+  const remove = wrapLoading(id => dispatch(removeStudent(id)));
 
-  const remove = id => dispatch(removeStudent(id));
+  const classes = useStyles();
 
   return (
     <>
       <CssBaseline />
-      <Container maxWidth="sm">
+      {loading && <LinearProgress variant="query" className={classes.loader} />}
+      <Container maxWidth="sm" className={classes.container}>
         <Box display="flex" justifyContent="flex-end" p={1}>
           <Tooltip title="Add a student" aria-label="add">
             <Fab onClick={openAdd} color="primary" aria-label="add">
@@ -75,7 +100,7 @@ function App() {
             </Fab>
           </Tooltip>
         </Box>
-        {students.length && (
+        {students.length !== 0 && (
           <List>
             {addingStudent && (
               <StudentForm
