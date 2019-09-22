c2367ee6c1358d608d5921019c0764ca0b118bba
Maxime Chasles <maxime.chasles@gmail.com>
2019-09-22 19:38:23 +0200

    delete default cra styles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
-import './index.css';
 import studentsReducer from './students.reducer';
 
 import App from './App';
